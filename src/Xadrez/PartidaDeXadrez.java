a41c59778dc5d9547174b6cc40101f1e9276909b
GabrielPSMartins <martins80gabriel@gmail.com>
2025-01-17 15:36:42 -0300

    Implementando o Bispo

--- src/Xadrez/PartidaDeXadrez.java
+++ src/Xadrez/PartidaDeXadrez.java
@@ -3,6 +3,7 @@ package Xadrez;
 import TabuleiroDoJogo.Peca;
 import TabuleiroDoJogo.Posicao;
 import TabuleiroDoJogo.Tabuleiro;
+import Xadrez.pecas.Bispo;
 import Xadrez.pecas.Peao;
 import Xadrez.pecas.Rei;
 import Xadrez.pecas.Torre;
@@ -193,7 +194,9 @@ public class PartidaDeXadrez {
 
     private void setupInicial() {
         colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCAS));
+        colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCAS));
         colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCAS));
+        colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCAS));
         colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCAS));
         colocarNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCAS));
         colocarNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCAS));
@@ -205,7 +208,9 @@ public class PartidaDeXadrez {
         colocarNovaPeca('h', 2, new Peao(tabuleiro, Cor.BRANCAS));
 
         colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETAS));
+        colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.BRANCAS));
         colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETAS));
+        colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.BRANCAS));
         colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETAS));
         colocarNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETAS));
         colocarNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETAS));
---
+++ src/Xadrez/pecas/Bispo.java
@@ -0,0 +1,64 @@
+package Xadrez.pecas;
+
+import TabuleiroDoJogo.Posicao;
+import TabuleiroDoJogo.Tabuleiro;
+import Xadrez.Cor;
+import Xadrez.PecaDeXadrez;
+
+public class Bispo extends PecaDeXadrez {
+
+    public Bispo(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "B";
+    }
+
+    @Override
+    public boolean[][] possiveisMovimentos() {
+        boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+        Posicao p = new Posicao(0, 0);
+
+        p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() - 1, p.getColuna() - 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && exisePecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() - 1, p.getColuna() + 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && exisePecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() + 1, p.getColuna() + 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && exisePecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().pecaExiste(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha() + 1, p.getColuna() - 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && exisePecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        return mat;
+    }
+}
+
